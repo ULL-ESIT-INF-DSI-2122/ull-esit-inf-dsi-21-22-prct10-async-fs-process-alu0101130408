51d9f5e7493f3b8c4be87ba42b7730f5a048b1b2
Joel Escobar <alu0101130408@ull.edu.es>
2022-04-28 20:33:06 +0000

    intento de test

---
+++ tests/ejercicio-1.spec.ts
@@ -0,0 +1,8 @@
+import 'mocha';
+// import {expect} from 'chai';
+
+describe('pruebas unitarias del ejercicio 1', ()=> {
+  it('Se supone que aqui van las pruebas del ejercico 1', ()=> {
+    console.log(`Como en el ejercicio 1 solo hay que comentar el funcionamiento de un programa. No se ha podido realizar pruebas unitarias`);
+  });
+});
